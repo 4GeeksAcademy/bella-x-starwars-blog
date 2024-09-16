87a117ea42814884695c297c9908810428f0480d
bellaxscobie <bellaxscobie@gmail.com>
2024-09-16 23:37:14 +0000

    Started project

---
+++ src/js/component/characters.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+export const Characters = () => (
+    <div className="container text-center mt-5">
+        <div className="card" style={{ width: "18rem" }}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                    Go somewhere
+                </a>
+            </div>
+        </div>
+        <div className="card" style={{ width: "18rem" }}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                    Go somewhere
+                </a>
+            </div>
+        </div>
+        <div className="card" style={{ width: "18rem" }}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                    Go somewhere
+                </a>
+            </div>
+        </div>
+        <div className="card" style={{ width: "18rem" }}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                    Go somewhere
+                </a>
+            </div>
+        </div>
+    </div>
+);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,23 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useState, useEffect } from "react";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.swapi.tech/api/people/")
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data.results); 
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div className="container text-center mt-5">
+      <h1>Star Wars Databank</h1>
+      <p className="lead">Explore the Star Wars universe. Browse characters, vehicles, and planets.</p>
+
+    </div>
+  );
+};
+
